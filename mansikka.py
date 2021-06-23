a2e6570034680c83fe5093b47d8c16a8b269407a
Jaakko <jaakko.lipponen01@gmail.com>
2021-06-23 21:58:43 +0300

    f

--- mansikka.py
+++ mansikka.py
@@ -1,3 +1,6 @@
+###################
+#### main file ####
+###################
 #illegal goods import
 import openpyxl, tkinter as tk, tkinter.font as tkFont, serial, datetime
 
@@ -10,10 +13,11 @@ window.geometry("1280x720")
 window.configure(bg='white')
 fontStyle = tkFont.Font(family="Lucida Grande", size=20)
 #functional
-label1 = tk.Label(text="Scale-inator")
-
+label = tk.Label(text="Scale-inator", bg="white")
+input1 = tk.Entry(width=50)
+id = input.get()
+weight =
 #compile window
-label1.pack()
-line = tk.Entry()
-line.pack()
+label.pack()
+input.pack()
 window.mainloop()
