6c8b9c8c9e00da9f82a47b3be0c01b1a773f4a74
Jaakko <jaakko.lipponen01@gmail.com>
2021-06-24 00:57:16 +0300

    Create UI.py

---
+++ UI.py
@@ -0,0 +1,19 @@
+#GUI
+import openpyxl, tkinter as tk, tkinter.font as tkFont, serial, datetime
+
+
+#init
+window = tk.Tk()
+#window size,colour,font
+window.geometry("1280x720")
+window.configure(bg='white')
+fontStyle = tkFont.Font(family="Lucida Grande", size=20)
+#functional
+label = tk.Label(text="Scale-inator", bg="white")
+input1 = tk.Entry(width=50)
+id = input.get()
+weight =
+#compile window
+label.pack()
+input.pack()
+window.mainloop()
