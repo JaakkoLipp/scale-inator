0de5a1cd2c01480b42c4f8ea89f3f17a642c15af
Jaakko <jaakko.lipponen01@gmail.com>
2021-06-24 16:17:46 +0300

    fixed if elses and autism

--- mansikka.py
+++ mansikka.py
@@ -5,53 +5,63 @@
 #illegal goods import
 import serial, datetime, sys, random
 import time
-#serial init
-"""ser = serial.Serial(
-    port='/dev/ttyUSB0',\
-    baudrate=9600,\
-    parity=serial.PARITY_NONE,\
-    stopbits=serial.STOPBITS_ONE,\
-    bytesize=serial.EIGHTBITS,\
-    timeout=0)"""
-
 
 def serRep(): #replaces serial input for demo
     return round(random.uniform(1,6), 2)
 
 ## MAIN
 def main():
-    prevID = ""
-    try:
+    #serial init
+    """ser = serial.Serial(
+        port='/dev/ttyUSB0',\
+        baudrate=9600,\
+        parity=serial.PARITY_NONE,\
+        stopbits=serial.STOPBITS_ONE,\
+        bytesize=serial.EIGHTBITS,\
+        timeout=0)"""
+    prevID = None
+
+    try:#while loop failure handling
         while(1):
-            try:
+            try: #scanning failure handling
                 currentID = input("SCAN ID, Q to exit and save: ")
+                #safe quit
                 if currentID == "q":
                     print("Saving...")
                     #file close
+                    #serial close
+                    """ser.close()"""
                     break
-                else:
+                #no quit
+                else: #convert id to int
                     currentID=int(currentID)
+            #scan fails:
             except:
                 print("SCAN FAILED")
                 continue
-            time.sleep(1)
+            time.sleep(0.2)#enhances reading clarity
+
             #serial read weight on scale
             """weight = ser.readline()"""
             weight = serRep() #read "serial"
+            ##ID processing
             if currentID == prevID: # scam prevention, check same persons all baskets pls
                 print("SAME AS PREVIOUS,\nNOT ACCEPTED.")
                 continue
-            collector = ((currentID-1)//20)+1
+
+            #prints & collector
+            collector = ((currentID-1)//20)+1 #calculates collector from ID
             print("#",str(collector)+",","SUCCESSFULLY SAVED", weight, "kg")
             #csv write weight+ID same row different columns
             prevID = currentID
-    except:
+
+    except: #save closing with file & serial
         print("Unexpected error:", sys.exc_info()[0])
         print("Program failed, closing and saving...")
         #file close
+        #serial close
+        """ser.close()"""
         sys.exit(-1)
 
-main()
-
-#serial close
-"""ser.close()"""
+if __name__ == "__main__":
+    main()
