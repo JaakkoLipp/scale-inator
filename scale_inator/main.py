39136e81d82ab82c8c64d72506e4912f8fda15c8
Jaakko <jaakko.lipponen01@gmail.com>
2021-06-24 18:41:17 +0300

    Update main.py

--- scale_inator/main.py
+++ scale_inator/main.py
@@ -28,7 +28,8 @@ def serRep():
     '''
     Serial like weight generator, Replaces serial input for demo
     '''
-    return round(random.uniform(10, 60), 2) #unrealistic debug numbers
+    # unrealistic debug numbers
+    return round(random.uniform(10, 60), 2)
 
 
 def cleanup():
