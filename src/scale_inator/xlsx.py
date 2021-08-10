6f24a50d467c9a1d5e622faec27a7a28b40ba807
Alfred Wingate <parona@protonmail.com>
2021-08-11 02:07:37 +0300

    revamp and optimize xlsxgen
    Signed-off-by: Alfred Wingate <parona@protonmail.com>

--- src/scale_inator/xlsx.py
+++ src/scale_inator/xlsx.py
@@ -1,3 +1,4 @@
+from numpy import float32, uint8
 import os
 import re
 import pandas
@@ -8,46 +9,81 @@ except ImportError:
 
 
 def create_xlsx():
-    files = []
+    '''
+    Create xlsx from csv files.
+    '''
+
+    csv_files = []
     csvregex = re.compile("data-([0-9]{4})([0-9]{2})([0-9]{2}).csv")
+
     for entry in os.listdir(data.xdg_data_dir()):
         if not csvregex.fullmatch(entry):
             continue
-        files = files + [os.path.join(data.xdg_data_dir(), entry)]
-    dataframe = pandas.concat([pandas.read_csv(
-        f, header=None,
-        parse_dates=[3],
-        names=["KG", "KoppaID", "CollectorID", "Date"]) for f in files],
-                              ignore_index=True)
+        csv_files = csv_files + [os.path.join(data.xdg_data_dir(), entry)]
 
-    # Yes, am iterating pandas dataframes. It works for now :|
-    summersum = pandas.DataFrame(columns=['CollectorID', 'KG'])
-    summersum = summersum.astype({'CollectorID': int, 'KG': float})
-    max_collector = dataframe["CollectorID"].max()
-    for col_num in range(1, max_collector+1):
-        temp = dataframe.copy()
-        temp = temp.query('(CollectorID == ' + str(col_num) + ')')
-        if temp.empty:
-            continue
-        else:
-            summersum = summersum.append(
+    try:
+        csv_combined = pandas.concat(
+            [
+                pandas.read_csv(
+                    file,
+                    header=None,
+                    parse_dates=[3],
+                    dtype={
+                        "KG": float32,
+                        "KoppaID": uint8,
+                        "CollectorID": uint8
+                    },
+                    names=["KG", "KoppaID", "CollectorID", "Date"]
+                ) for file in csv_files
+            ],
+            ignore_index=True
+        )
+    except ValueError:
+        raise ValueError("CSV column has incorrect data")
+
+    summersum = pandas.DataFrame(columns=["Kerääjä ID", "Paino"])
+    summersum = summersum.astype({"Kerääjä ID": uint8, "Paino": float32})
+
+    for id in csv_combined["CollectorID"].unique():
+        summersum = summersum.append(
+            pandas.DataFrame(
+                [
+                    {
+                        "Kerääjä ID": id,
+                        "Paino": csv_combined.where(
+                            csv_combined["CollectorID"] == id
+                        )["KG"].sum()
+                    }
+                ]
+            ).astype(
                 {
-                    'CollectorID': col_num,
-                    'KG': temp["KG"].sum()
-                },
-                ignore_index=True)
-    pandas.to_numeric(summersum['CollectorID'], downcast='integer')
+                    "Kerääjä ID": uint8,
+                    "Paino": float32
+                }
+            ),
+            ignore_index=True
+        )
 
     location = os.path.join(data.xdg_data_dir(), "mansikanpoiminta.xlsx")
 
     writer = pandas.ExcelWriter(location, engine="xlsxwriter")
-    summersum.to_excel(writer, sheet_name="Summer sum", index=False)
+    summersum.to_excel(
+        writer,
+        sheet_name="Koko kesän kerätty määrä",
+        index=False
+    )
 
     workbook = writer.book
-    worksheet = writer.sheets['Summer sum']
+    worksheet = writer.sheets["Koko kesän kerätty määrä"]
 
     # Must escape g in format so that excel doesnt reject it
     kilogram_format = workbook.add_format(
-        {"num_format": "0.00k\g"})  # noqa: W605
+        {
+            "num_format": "0.00k\g"  # noqa: W605
+        }
+    )
+
+    worksheet.set_column("A:A", len("Kerääjä ID"))
+    worksheet.set_column("B:B", 10, kilogram_format)  # arbitary width
 
     workbook.close()
